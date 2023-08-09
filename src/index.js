f6e161c26438b1eb35e986d9aea5f01a2aced741
mar69287 <marcoa.ruiz97@gmail.com>
2023-08-09 16:12:10 -0700

    style non-user navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ChakraProvider,  extendTheme } from '@chakra-ui/react'
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import NonUserNavBar from "../components/NavBar/NonUserNavBar"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <NonUserNavBar />
   )
 }
 
