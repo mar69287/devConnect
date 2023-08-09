66c642d8fbc95746aed255dfabab3a4754a05dfa
mar69287 <marcoa.ruiz97@gmail.com>
2023-08-09 14:52:52 -0700

    Install chakra

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App/App';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ChakraProvider,  extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    body: "PT Serif, serif",
+    heading: "Roboto, sans-serif",
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Router>
   </React.StrictMode>
 );
