8f485eab44217f03978d035d9f3659de49163554
mar69287 <marcoa.ruiz97@gmail.com>
2023-08-17 18:28:43 -0700

    add logo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,18 @@
-import { AbsoluteCenter, Box, VStack, HStack, Heading } from '@chakra-ui/react'
+import { AbsoluteCenter, Box, VStack, HStack, Heading, Image } from '@chakra-ui/react'
 import LoginForm from '../components/LoginForm/LoginForm'
+import logo from '../assets/logo.png'
 
 export default function AuthPage({ setUser }) {
     return (
         <Box as='main' w={'100vw'} h={'100vh'}>
             <AbsoluteCenter>
-                <HStack w={'100vw'} p={5}>
+                <HStack width={{base: '100vw', '2xl': '1400px'}} p={5}>
                     <VStack flex={1}>
                         <Heading color={'white'}>Welcome Back!</Heading>
                         <LoginForm setUser={setUser} />
                     </VStack>
                     <Box flex={1}>
-
+                        <Image src={logo} />
                     </Box>
                 </HStack>
             </AbsoluteCenter>
