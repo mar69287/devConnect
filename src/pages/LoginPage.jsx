75a5fea4ba64126ac330dc96744a50efadf97024
mar69287 <marcoa.ruiz97@gmail.com>
2023-08-17 18:22:16 -0700

    edit login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,20 @@
+import { AbsoluteCenter, Box, VStack, HStack, Heading } from '@chakra-ui/react'
 import LoginForm from '../components/LoginForm/LoginForm'
 
 export default function AuthPage({ setUser }) {
     return (
-        <main>
-            {/* <h1>AuthPage</h1>
-            <LoginForm setUser={setUser} /> */}
-        </main>
+        <Box as='main' w={'100vw'} h={'100vh'}>
+            <AbsoluteCenter>
+                <HStack w={'100vw'} p={5}>
+                    <VStack flex={1}>
+                        <Heading color={'white'}>Welcome Back!</Heading>
+                        <LoginForm setUser={setUser} />
+                    </VStack>
+                    <Box flex={1}>
+
+                    </Box>
+                </HStack>
+            </AbsoluteCenter>
+        </Box>
     )
 }
