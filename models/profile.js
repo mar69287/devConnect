4e7a49f71eb3db869876ec0da3a430201100df7f
mar69287 <marcoa.ruiz97@gmail.com>
2023-08-21 17:21:04 -0700

    create profile

---
+++ models/profile.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const skillSchema = new Schema({
+    skill: {
+        type: String,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const profileSchema = new Schema({
+    location: String,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    bio: {
+        type: String,
+        maxlength: 100,
+    },
+    github: String,
+    linkedIn: String,
+    portfolio: String,
+    picture: String,
+    skills: [skillSchema],
+    following: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Profile'
+    }],
+    followers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Profile'
+    }],
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Profile', profileSchema);
