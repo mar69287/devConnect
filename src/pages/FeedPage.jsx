6744489f7e364eefd94a0685b7299dae4330794d
mar69287 <marcoa.ruiz97@gmail.com>
2023-08-24 18:09:58 -0700

    remove random letters

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -51,11 +51,11 @@ const FeedPage = ({ profile, setProfile }) => {
             </VStack>
             <VStack w={['100%', '50%']} >
                 <VStack gap={0} py={3} px={4} backgroundColor={'rgb(28, 30, 35)'} borderColor={'WhiteAlpha300'} border={'2px solid'} borderRadius={10} w={'100%'}>
-                    <HStack mb={4} w={'100%'} dis>
+                    <HStack mb={4} w={'100%'} >
                         <Image
                             borderRadius='full'
                             boxSize='40px'
-                            src={profile.picture ? `/assets/${profile.picture}` : 'https://bit.ly/dan-abramov'}s
+                            src={profile.picture ? `/assets/${profile.picture}` : 'https://bit.ly/dan-abramov'}
                             alt='Dan Abramov'
                             // mt={1}
                             border={'2px solid'}
