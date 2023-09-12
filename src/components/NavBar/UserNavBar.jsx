c1ab445e55da22431e324ad7ad074139c1ffb904
mar69287 <marcoa.ruiz97@gmail.com>
2023-09-11 19:18:58 -0700

    add links

--- src/components/NavBar/UserNavBar.jsx
+++ src/components/NavBar/UserNavBar.jsx
@@ -14,37 +14,34 @@ export default function NavBar({ user, setUser }) {
     }
 
     return (
-        // <nav>
-        //     <Link to="/orders">Order History</Link>
-        //     &nbsp; | &nbsp;
-        //     <Link to="/orders/new">New Order</Link>
-        //     &nbsp;&nbsp; Welcome, {user.name}
-        //     &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
-        // </nav>
         <Box w={'100%'} position={'fixed'} top={0} left={0} zIndex={3} backgroundColor={'rgb(15, 16, 19)'}>
             <HStack width={{lg: '100%', '2xl': '1400px'}} m='5px auto' justifyContent={'space-between'} p={'.9rem 1.8rem .9rem 1.4rem'}>
-                <HStack>
-                    <Heading _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}} as={'h1'} fontSize={{ base: '1.1rem', md: '1.3rem', lg: '1.5rem' }} color="rgb(255, 255, 255)" p={'.3rem .1rem'} borderColor={'rgb(255, 255, 255)'} borderBottom={'2px solid'} borderTop={'2px solid'}>
-                        devConnect
-                    </Heading>
-                </HStack>
+                <Link to="/feed">
+                    <HStack>
+                        <Heading _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}} as={'h1'} fontSize={{ base: '1.1rem', md: '1.3rem', lg: '1.5rem' }} color="rgb(255, 255, 255)" p={'.3rem .1rem'} borderColor={'rgb(255, 255, 255)'} borderBottom={'2px solid'} borderTop={'2px solid'}>
+                            devConnect
+                        </Heading>
+                    </HStack>
+                </Link>
                 <HStack gap={4}>
-                    <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
-                        <AiFillHome fontSize={20}/>
-                        <Text fontSize={15}>Home</Text>
-                    </VStack>
-                    <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
-                        <BsPeopleFill fontSize={20}/>
-                        <Text fontSize={15}>Friends</Text>
-                    </VStack>
-                    <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
-                        <BiSolidMessageRoundedDetail fontSize={20}/>
-                        <Text fontSize={15}>Messages</Text>
-                    </VStack>
-                    {/* <HStack className="login-stack" _hover={{cursor: 'pointer', backgroundColor: 'rgb(26, 29, 35)', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}} gap={0} color={'rgb(204, 206, 209)'} p={'.3rem 1rem'} backgroundColor={'rgb(29, 33, 39)'} borderRadius={50}>
-                        <Text>Profile</Text>
-                        <MdKeyboardArrowDown className="arrowdown-icon"/>
-                    </HStack> */}
+                    <Link to="/feed">
+                        <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
+                            <AiFillHome fontSize={20}/>
+                            <Text fontSize={15}>Home</Text>
+                        </VStack>
+                    </Link>
+                    <Link to="/friends">
+                        <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
+                            <BsPeopleFill fontSize={20}/>
+                            <Text fontSize={15}>Friends</Text>
+                        </VStack>
+                    </Link>
+                    <Link to="/messaging">
+                        <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
+                            <BiSolidMessageRoundedDetail fontSize={20}/>
+                            <Text fontSize={15}>Messages</Text>
+                        </VStack>
+                    </Link>
                     <Menu>
                         <MenuButton
                             className="login-stack"
@@ -72,7 +69,6 @@ export default function NavBar({ user, setUser }) {
                             </MenuItem>
                             <MenuDivider />
                             <MenuItem onClick={handleLogOut}>
-                                {/* <Link  to="" onClick={handleLogOut}><Text w={"100%"}>Log Out</Text></Link> */}
                                 Log Out
                             </MenuItem>
                         </MenuList>
