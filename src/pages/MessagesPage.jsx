36ac812c1c58df86b76f7b7dfc1297d2609eb76c
mar69287 <marcoa.ruiz97@gmail.com>
2023-09-15 11:23:23 -0700

    remove comments

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const MessagesPage = ({ chatUser }) => {
-    // console.log(process.env.REACT_APP_CHAT_ENGINE_API_KEY)
+
   return (
 
       <Box w={'100%'} position={'fixed'} top={'90px'} left={0} zIndex={0} h={'93%'}>
