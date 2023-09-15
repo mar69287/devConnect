a079482df8f4fb4b5cb64414bbc52b1dd2d8f37e
mar69287 <marcoa.ruiz97@gmail.com>
2023-09-15 10:38:03 -0700

    responsive

--- src/components/NavBar/UserNavBar.jsx
+++ src/components/NavBar/UserNavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Box, HStack, Heading, Text, VStack, Menu, MenuButton, MenuList, MenuItem, Button, MenuDivider, Spinner } from '@chakra-ui/react'
+import { Box, HStack, Heading, Text, VStack, Menu, MenuButton, MenuList, MenuItem, Button, MenuDivider, Spinner, Hide, Show } from '@chakra-ui/react'
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { AiFillHome } from 'react-icons/ai'
 import { BsPeopleFill } from 'react-icons/bs'
@@ -20,28 +20,41 @@ export default function NavBar({ user, setUser, profile }) {
             <HStack width={{lg: '100%', '2xl': '1400px'}} m='5px auto' justifyContent={'space-between'} p={'.9rem 1.8rem .9rem 1.4rem'}>
                 <Link to="/feed">
                     <HStack>
-                        <Heading _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}} as={'h1'} fontSize={{ base: '1.1rem', md: '1.3rem', lg: '1.5rem' }} color="rgb(255, 255, 255)" p={'.3rem .1rem'} borderColor={'rgb(255, 255, 255)'} borderBottom={'2px solid'} borderTop={'2px solid'}>
-                            devConnect
-                        </Heading>
+                        <Hide breakpoint='(max-width: 500px)'>
+                            <Heading _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}} as={'h1'} fontSize={{ base: '1.1rem', md: '1.3rem', lg: '1.5rem' }} color="rgb(255, 255, 255)" p={'.3rem .1rem'} borderColor={'rgb(255, 255, 255)'} borderBottom={'2px solid'} borderTop={'2px solid'}>
+                                devConnect
+                            </Heading>
+                        </Hide>
+                        <Show breakpoint='(max-width: 500px)'>
+                            <Heading _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}} as={'h1'} fontSize={{ base: '1.1rem', md: '1.3rem', lg: '1.5rem' }} color="rgb(255, 255, 255)" p={'.3rem .1rem'} borderColor={'rgb(255, 255, 255)'} borderBottom={'2px solid'} borderTop={'2px solid'}>
+                                DC
+                            </Heading>
+                        </Show>
                     </HStack>
                 </Link>
                 <HStack gap={4}>
-                    <Link to="/feed">
-                        <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
-                            <AiFillHome fontSize={20}/>
-                            <Text fontSize={15}>Home</Text>
-                        </VStack>
-                    </Link>
+                        <Link to="/feed">
+                            <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
+                                <AiFillHome fontSize={20}/>
+                                <Hide breakpoint='(max-width: 375px)'>
+                                    <Text fontSize={15}>Home</Text>
+                                </Hide>
+                            </VStack>
+                        </Link>
                     <Link to="/friends">
                         <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
                             <BsPeopleFill fontSize={20}/>
-                            <Text fontSize={15}>Friends</Text>
+                            <Hide breakpoint='(max-width: 375px)'>
+                                <Text fontSize={15}>Friends</Text>
+                            </Hide>
                         </VStack>
                     </Link>
                     <Link to="/messaging">
                         <VStack color={'rgb(204, 206, 209)'} gap={0} _hover={{cursor: 'pointer', color: 'rgb(183, 184, 185)', transition: 'all 0.3s ease-in-out'}}>
                             <BiSolidMessageRoundedDetail fontSize={20}/>
-                            <Text fontSize={15}>Messages</Text>
+                            <Hide breakpoint='(max-width: 375px)'>
+                                <Text fontSize={15}>Messages</Text>
+                            </Hide>
                         </VStack>
                     </Link>
                     {profile ? (<Menu>
@@ -60,10 +73,11 @@ export default function NavBar({ user, setUser, profile }) {
                             _expanded={{ bg: 'rgb(26, 29, 35)' }}
                             borderRadius={50}
                             as={Button}
-                            rightIcon={<MdKeyboardArrowDown className="arrowdown-icon" />}
                         >
-                            {profile.name}
-    
+                            <HStack>
+                                <Hide breakpoint='(max-width: 500px)'>{profile.name}</Hide>
+                                <MdKeyboardArrowDown className="arrowdown-icon" />
+                            </HStack>
                         </MenuButton>
                         <MenuList>
                                 <Link to={`/profile/${profile.userName}`}>
