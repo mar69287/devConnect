df3d844d212d386987a7f1b8ef187751e68be96c
mar69287 <marcoa.ruiz97@gmail.com>
2023-09-22 10:07:03 -0700

    remove unused elements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-import { Box, Center, Heading, Text, VStack } from "@chakra-ui/react"
 import Hero from "../components/Hero/Hero"
 import NonUserNavBar from "../components/NavBar/NonUserNavBar"
 import Target from "../components/Target"
