e5ebfc5afd535830179b467320a97bdb1167ac8e
mar69287 <marcoa.ruiz97@gmail.com>
2023-10-17 10:48:57 -0700

    update styling

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -5,7 +5,7 @@ const MessagesPage = ({ chatUser }) => {
 
   return (
 
-      <Box w={'100%'} position={'fixed'} top={'90px'} left={0} zIndex={0} h={'93%'}>
+      <Box w={'100%'} position={'fixed'} top={'90px'} left={0} zIndex={0} h={'85%'}>
           <PrettyChatWindow
             projectId={`${process.env.REACT_APP_CHAT_ENGINE_API_KEY}`}
             username={chatUser.username}
