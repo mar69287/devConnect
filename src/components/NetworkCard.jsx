8cadb88f9ffe1e6f1e27f0dc31103145acee7dd0
mar69287 <marcoa.ruiz97@gmail.com>
2023-11-14 11:49:12 -0800

    new network card design

---
+++ src/components/NetworkCard.jsx
@@ -0,0 +1,122 @@
+import { Card, Heading, Image, VStack, Button } from "@chakra-ui/react"
+import { motion } from "framer-motion";
+import { deleteFollowing, addFollowing } from "../utilities/profiles-api";
+import { useState } from "react";
+
+const NetworkCard = ({ person, followingComponent, setFollowing, setFollowingCount, profile, following }) => {
+    const [isFollowingUser, setIsFollowingUser] = useState(following.some((followedProfile) => followedProfile._id === person._id));
+
+    const handleDeleteFollowing = async (followerId) => {
+        try {
+            await deleteFollowing(profile._id, followerId);
+            setFollowing((prevFollowing) => prevFollowing.filter((profile) => profile._id !== followerId));
+            setFollowingCount((prevTotal) => prevTotal - 1);
+        } catch (error) {
+            console.error("Error deleting follow:", error);
+        }
+    }
+
+    const handleAddFollower = async (followerId) => {
+        try {
+            const newFollowing = await addFollowing(profile._id, followerId);
+            console.log(newFollowing)
+            setFollowing((prev) => [...prev, newFollowing]);
+            setFollowingCount((prevTotal) => prevTotal + 1);
+            setIsFollowingUser(true)
+        } catch (error) {
+            console.error("Error adding follow:", error);
+        }
+    }
+
+    const handleDeleteFollower = async (followerId) => {
+        console.log('in delete follower')
+    }
+
+  return (
+    <Card bgGradient='linear(to-l, #1A1A1A, #FFF, #1A1A1A)' py={'.5px'} h={'15rem'} w={'13rem'} borderRadius={3} border={'1px solid #1A1A1A'} _hover={{paddingY: '1px'}}>
+        <VStack w={'100%'} h={'100%'} bg={'rgb(15, 16, 19)'} justifyContent={'center'} gap={3}>
+            <Image
+                borderRadius='full'
+                boxSize='50px'
+                src={person.picture ? `${person.picture}` : 'https://i.imgur.com/uNL6B8O.png'}
+                alt='Dan Abramov'
+                border={'2px solid'}
+                borderColor={"whiteAlpha.600"}
+            />
+            <Heading align={'center'}  fontWeight={'normal'} fontSize={['sm', 'md', 'lg']} color="rgb(255, 255, 255)">{person.userName}</Heading>
+            {followingComponent ? (
+                <motion.div
+                    style={{
+                      padding: '.5rem 1rem',
+                      backgroundColor: '#FFF',
+                      display: 'flex',
+                      fontWeight: 500,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      borderRadius: 5,
+                      position: 'relative',
+                      overflow: 'hidden', 
+                      gap: 5
+                    }}
+                    whileHover={{
+                      scale: 1.02,
+                      background: 'linear-gradient(to right, #7928CA, #FF0080)',
+                      color: '#FFF',
+                      cursor: 'pointer'
+                    }}
+                    whileTap={{
+                      scale: .9,
+                    }}
+                    onClick={() => handleDeleteFollowing(person._id)}
+                  >
+                    Unfollow
+                  </motion.div>
+            ) : (
+                <motion.div
+                style={{
+                  padding: '.5rem 1rem',
+                  backgroundColor: '#FFF',
+                  display: 'flex',
+                  fontWeight: 500,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: 5,
+                  position: 'relative',
+                  overflow: 'hidden', 
+                  gap: 5
+                }}
+                whileHover={{
+                  scale: 1.02,
+                  background: 'linear-gradient(to right, #7928CA, #FF0080)',
+                  color: '#FFF',
+                  cursor: 'pointer'
+                }}
+                whileTap={{
+                  scale: .9,
+                }}
+                onClick={() => {
+                    if (isFollowingUser) {
+                        handleDeleteFollower(person._id);
+                    } else {
+                        handleAddFollower(person._id);
+                    }
+                }}
+              >
+                {isFollowingUser ? "Following" : "Follow"}
+              </motion.div>
+            )}
+            {/* <Button bgColor={'#FFF'} fontWeight={500}>
+                Unfollow
+            </Button> */}
+            
+        </VStack>
+    </Card>
+    // <Card bgGradient='linear(to-l, #1A1A1A, #FFF, #1A1A1A)' pb={'.5px'} h={'12rem'} w={'10rem'} key={person._id} borderRadius={3} border={'1px solid #1A1A1A'} _hover={{paddingBottom: '1px'}}>
+    //     <Box w={'100%'} h={'100%'} bg={'rgb(15, 16, 19)'} >
+
+    //     </Box>
+    // </Card>
+  )
+}
+
+export default NetworkCard
--- src/pages/NetworkPage.jsx
+++ src/pages/NetworkPage.jsx
@@ -1,11 +1,13 @@
-import { Grid, GridItem, Heading, Button, VStack, SimpleGrid, Text, HStack } from "@chakra-ui/react"
+import { Grid, GridItem, Heading, Button, VStack, SimpleGrid, Text, HStack, Card, Box } from "@chakra-ui/react"
 import { FaPeopleGroup } from 'react-icons/fa6'
 import { BsFillPeopleFill } from 'react-icons/bs'
 import PersonCard from "../components/PersonCard"
 import { useState } from "react"
+import { motion } from "framer-motion";
+import NetworkCard from "../components/NetworkCard"
 
 
-const NetworkPage = ({ profile, followers, followersCount, setFollowersCount, following, setFollowing, followingCount }) => {
+const NetworkPage = ({ profile, followers, followersCount, setFollowersCount, following, setFollowing, followingCount, setFollowingCount }) => {
   const [followingComponent, setFollowingComponent] = useState(true)
 
   return (
@@ -52,34 +54,39 @@ const NetworkPage = ({ profile, followers, followersCount, setFollowersCount, fo
             </VStack>
       </GridItem>
 
-      <GridItem  h={'max-content'} as="main" py={3} px={4} backgroundColor={'rgb(28, 30, 35)'} borderColor={'WhiteAlpha300'} border={'2px solid'} borderRadius={10}>
-        <SimpleGrid spacing={4} columns={[1]} w={'100%'}>
+      {/* <GridItem  h={'max-content'} as="main" py={3} px={4} backgroundColor={'rgb(28, 30, 35)'} borderColor={'WhiteAlpha300'} border={'2px solid'} borderRadius={10}> */}
+      <GridItem  h={'max-content'} as="main" py={3} px={4}>
             {followingComponent ? (
-                <Text align={'left'} color={"transparent"} bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize={'1.3rem'}>Following</Text>
+                <Text mb={5} align={'left'} color={"transparent"} bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize={'1.3rem'}>Following</Text>
             ) : (
-                <Text align={'left'} bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize={'1.3rem'}>Followers</Text>
+                <Text mb={5} align={'left'} bgGradient='linear(to-l, #7928CA, #FF0080)' bgClip='text' fontSize={'1.3rem'}>Followers</Text>
                 
             )} 
+        <SimpleGrid spacing={4} minChildWidth={'13rem'} w={'100%'} alignItems={'center'} justifyItems={'center'} >
             {followingComponent && following.map((person) => (
-                <PersonCard
-                    key={person._id}
-                    person={person}
-                    following={following}
-                    setFollowing={setFollowing}
-                    setFollowingCount={setFollowersCount}
-                    profile={profile}
-                />
+                // <PersonCard
+                //     key={person._id}
+                //     person={person}
+                //     following={following}
+                //     setFollowing={setFollowing}
+                //     setFollowingCount={setFollowersCount}
+                //     profile={profile}
+                // />
+                <NetworkCard key={person._id} person={person} followingComponent={followingComponent} profile={profile} setFollowing={setFollowing} setFollowingCount={setFollowingCount} following={following} />
+
             ))}
 
             {!followingComponent && followers.map((person) => (
-                <PersonCard
-                    key={person._id}
-                    person={person}
-                    following={following}
-                    setFollowing={setFollowing}
-                    setFollowingCount={setFollowersCount}
-                    profile={profile}
-                />
+                // <PersonCard
+                //     key={person._id}
+                //     person={person}
+                //     following={following}
+                //     setFollowing={setFollowing}
+                //     setFollowingCount={setFollowersCount}
+                //     profile={profile}
+                // />
+                <NetworkCard key={person._id} person={person} followingComponent={followingComponent} profile={profile} setFollowing={setFollowing} setFollowingCount={setFollowingCount} following={following} />
+
             ))}
         </SimpleGrid>
       </GridItem>
